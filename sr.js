dccf0b8e5a7e7d2a58dde7351ea9f68210a946f8
igortheprogrammer <e3341048@gmail.com>
2025-02-19 19:53:54 +0600

    Not the first first upload
    hello

---
+++ sr.js
@@ -0,0 +1,8 @@
+function getPCElement(){
+    let element = document.getElementById("element");
+    return(element.value);
+}
+// https://www.citilink.ru/search/?text=
+// https://www.dns-shop.ru/search/?q=
+// https://www.mvideo.ru/product-list-page?q=
+// https://www.eldorado.ru/search/catalog.php?q=
